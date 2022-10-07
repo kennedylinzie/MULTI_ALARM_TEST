eac63e53bd5ee5d762078744181d371a81ce943f
kennedy <kennedylinzie@gmail.com>
2022-10-07 21:25:17 +0200

    best modification yet use year,month,date,hour,min,sec

--- app/src/main/java/com/example/multi_alarm_test/MainActivity.java
+++ app/src/main/java/com/example/multi_alarm_test/MainActivity.java
@@ -1,32 +1,72 @@
- package com.example.multi_alarm_test;
+package com.example.multi_alarm_test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Debug;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.util.Date;
 import java.util.List;
 
- public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     Button btn_set_larm;
     TextView txt_timer;
+    // on below line we are creating a variable.
+    private TextView currentTV;
+    RelativeLayout back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // back = findViewById(R.id.backgamon);
+        // on below line we are initializing our variables.
+        currentTV = findViewById(R.id.idTVCurrent);
+
+
+        Calendar now = Calendar.getInstance();
+        //System.out.println(now.getTime());
+        Log.d("time", now.getTime()+"");
+        Log.d("time", now.get(Calendar.HOUR_OF_DAY)+"");
+
+
+
+        Handler handler = new Handler();
+        Runnable runnableCode = new Runnable() {
+            @Override
+            public void run() {
+                SimpleDateFormat sdf = new SimpleDateFormat("'Date\n'dd-MM-yyyy '\n\nand\n\nTime\n'HH:mm:ss z");
+                String currentDateAndTime = sdf.format(new Date());
+                currentTV.setText(currentDateAndTime);
+                handler.postDelayed(this, 1000);
+            }
+        };
+        handler.post(runnableCode);
+
+
+
 
         init();
     }
@@ -35,55 +75,86 @@ import java.util.List;
     public void init(){
 
         btn_set_larm = findViewById(R.id.button);
-        txt_timer = findViewById(R.id.textView);
+        // txt_timer = findViewById(R.id.textView);
 
-        btn_set_larm.setOnClickListener(view -> setAlarm(1));
+        btn_set_larm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+              /*  setAlarm(1,9,0);
+                setAlarm(1,9,1);
+                setAlarm(1,9,2);
+                setAlarm(1,9,3);
+                setAlarm(1,9,4);
+                setAlarm(1,9,5);*/
+                setAlarm(1,2022,10,7,16,32,0);
+                setAlarm(1,2022,10,7,16,33,0);
+                setAlarm(1,2022,10,7,16,34,0);
+                setAlarm(1,2022,10,7,16,35,0);
+                setAlarm(1,2022,10,7,16,36,0);
+                setAlarm(1,2022,10,7,16,37,0);
+                setAlarm(1,2022,10,7,16,38,0);
+
+            }
+        });
+
+
 
     }
+    PendingIntent pi;
+    Intent intent;
+    @SuppressLint("UnspecifiedImmutableFlag")
+    public void setAlarm(int number,int Y_set,int M_set,int D_set,int h_set,int m_set,int s_set){
 
-    public void setAlarm(int number){
+
 
         AlarmManager am  = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-       // Calendar calendar = GregorianCalendar.getInstance();
-        GregorianCalendar calendar = new GregorianCalendar();
-        List<GregorianCalendar> calendarList = new ArrayList<>();
+        Calendar calendar = Calendar.getInstance();
+        List<Calendar> calendarList = new ArrayList<>();
+
+        //calendar.set(Calendar.HOUR,0);
+        // calendar.set(Calendar.MINUTE,0);
+        // calendar.set(Calendar.MINUTE,0);
+        calendar.set(Y_set,M_set-1,D_set,h_set,m_set,s_set);
 
         for (int i = 0; i < number; i++) {
             calendarList.add(calendar);
         }
-        for (GregorianCalendar calendarItem:calendarList)
+        for (Calendar calendarItem:calendarList)
         {
             //each calender we will set alarm for it
-            //calendarItem.add(Calendar.SECOND,10);
-            //each calender we will set alarm for it
+            //calendarItem.add(CalendaR.GE);
+            // calendarItem.add(Calendar.MONTH,M_set);
+            // calendarItem.add(Calendar.DATE,D_set);
+            // calendarItem.add(Calendar.HOUR,h_set);
+            ///calendarItem.add(Calendar.MINUTE, m_set);
 
-           // calendarItem.add("2022");
-            /*calendarItem.add(GregorianCalendar.MONTH,);
-            calendarItem.add(GregorianCalendar.DAY_OF_MONTH,11);*/
-            calendarItem.add(Calendar.HOUR, 9+3);
-            calendarItem.add(GregorianCalendar.MINUTE, Integer.parseInt("0"));
-            calendarItem.add(GregorianCalendar.SECOND,Integer.parseInt("0"));
 
-            String Y =  calendarItem.get(GregorianCalendar.YEAR) + "";
-            String M = calendarItem.get(GregorianCalendar.MONTH) + "";
-            String D = calendarItem.get(GregorianCalendar.DAY_OF_MONTH)+"";
-            String h_ =  calendarItem.get(GregorianCalendar.HOUR) + "";
-            String m_ = calendarItem.get(GregorianCalendar.MINUTE) + "";
-            String s_ = calendarItem.get(GregorianCalendar.SECOND)+"";
+            String Y =  calendarItem.get(Calendar.YEAR) + "";
+            String M = calendarItem.get(Calendar.MONTH) + "";
+            String D = calendarItem.get(Calendar.DAY_OF_MONTH)+"";
+            String h_ =  calendarItem.get(Calendar.HOUR) + "";
+            String m_ = calendarItem.get(Calendar.MINUTE) + "";
+            String s_ = calendarItem.get(Calendar.SECOND)+"";
             String name = Y+" : "+M+" : "+D+" : "+h_+" : "+m_+" : "+s_;
+            String code = Y+""+M+""+D+""+h_+""+m_+""+s_;
+           String String = java.lang.String.valueOf(calendarItem.getTime());
             Toast.makeText(this, name+" ", Toast.LENGTH_SHORT).show();
 
 
-            int requestCode  = (int) GregorianCalendar.getInstance().getTimeInMillis()/1000;
-            Intent intent = new Intent(this,MyAlarmReceiver.class);
+            int requestCode  = Integer.parseInt(code);
+            intent = new Intent(this,MyAlarmReceiver.class);
             intent.putExtra("REQUEST_CODE",requestCode);
             intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
             intent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
+
             //HERE EACH ALARM WE WILL SET EACH 'request code',incase you want to cancel alarm, you can use this request code
             //because i get time in milis to requestcode ,it is long so i must div by 1000 to get integer
-            PendingIntent pi = PendingIntent.getBroadcast(this,requestCode,intent,0);
+            pi = PendingIntent.getBroadcast(this,requestCode,intent,PendingIntent.FLAG_ONE_SHOT);
+
+
             if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                am.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP,calendarItem.getTimeInMillis(),pi);
+                am.setExact(AlarmManager.RTC_WAKEUP,calendarItem.getTimeInMillis(),pi);
             }else{
                 am.setExact(AlarmManager.RTC_WAKEUP,calendarItem.getTimeInMillis(),pi);
             }
@@ -93,4 +164,28 @@ import java.util.List;
 
     }
 
+
+    @SuppressLint("UnspecifiedImmutableFlag")
+    public void cancel_alarm(View view) {
+        pi = PendingIntent.getBroadcast(this, 91, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        if(pi != null) {
+            alarmManager.cancel(pi);
+
+        }
+
+    }
+
+    @SuppressLint("UnspecifiedImmutableFlag")
+    public void check_if_existing(View view) {
+        pi = PendingIntent.getBroadcast(this, 91, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        try {
+            int requestCode = intent.getExtras().getInt("requestCode");
+            Toast.makeText(this, "the code is "+requestCode, Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+
+        }
+
+    }
 }
--- app/src/main/java/com/example/multi_alarm_test/MyAlarmReceiver.java
+++ app/src/main/java/com/example/multi_alarm_test/MyAlarmReceiver.java
@@ -3,13 +3,18 @@ package com.example.multi_alarm_test;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.widget.Toast;
 
 public class MyAlarmReceiver extends BroadcastReceiver {
-
     @Override
     public void onReceive(Context context, Intent intent) {
         int requestcode = intent.getIntExtra("REQUEST_CODE",-1);
-        Toast.makeText(context, "Alarm fired with requestcode", Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, ""+requestcode, Toast.LENGTH_LONG).show();
+        Uri alarmSound = RingtoneManager. getDefaultUri (RingtoneManager. TYPE_NOTIFICATION );
+        MediaPlayer mp = MediaPlayer. create (context.getApplicationContext(), alarmSound);
+        mp.start();
     }
 }
